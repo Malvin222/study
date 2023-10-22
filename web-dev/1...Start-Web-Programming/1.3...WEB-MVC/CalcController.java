490d3e4c1b36a307cf2d64c72fc81a9cb0169445
malvin222 <mahebin@naver.com>
2023-10-22 21:24:43 +0900

    add

---
+++ web-dev/1...Start-Web-Programming/1.3...WEB-MVC/CalcController.java
@@ -0,0 +1,24 @@
+package org.zerock.w1.calc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="calcController", urlPatterns = "/calc/makeResult")
+public class CalcController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+
+        String num1 = req.getParameter("num1");
+        String num2 = req.getParameter("num2");
+
+        System.out.println("num1:%s"+ num1);
+        System.out.println("num2:%s"+num2);
+
+        resp.sendRedirect("/index");
+    }
+}
---
+++ web-dev/1...Start-Web-Programming/1.3...WEB-MVC/calcResult.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: maheb
+  Date: 2023-10-19
+  Time: 오후 5:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>NUM1 ${param.num1}</h1>
+    <h1>NUM2 ${param.num2}</h1>
+
+    <h1> SUM ${Integer.parseInt(param.num2) + Integer.parseInt(param.num2)}</h1>
+
+</body>
+</html>
